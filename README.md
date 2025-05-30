ac4fbeede0744f0645a96099dab623991edbea3b
Sypchik <tastysypchik@proton.me>
2025-05-30 18:22:04 +0300

    update

--- README.md
+++ README.md
@@ -7,45 +7,44 @@ It is based on a Bash script that connects to a private GitLab instance via the 
 
 ## Features
 
-* Automatically detects all groups and subgroups.
-  
-* Exports all repositories within those groups.
+- Automatically detects all groups and subgroups.
+- Exports all repositories within those groups.
 
-* Tracks the export status and waits until it's ready for download.
+- Tracks the export status and waits until it's ready for download.
 
-* Handles GitLab rate limits for export/import requests.
+- Handles GitLab rate limits for export/import requests.
 
-* Logs actions with timestamps into export.log.
+- Logs actions with timestamps into export.log.
 
-* Respects user-defined export rate limits to avoid GitLab throttling.
+- Respects user-defined export rate limits to avoid GitLab throttling.
 
 ## Usage
 
 1. Make sure you have jq and curl installed:
 
-```bash
-sudo apt install jq curl
-```
+   ```bash
+   sudo apt install jq curl
+   ```
 
 2. Open export.sh and replace url, token, data dir, or set your GitLab instance URL, private token and custom dir:
 
-```bash
-export PRIVATE_TOKEN=your-access-token
-export GITLAB_URL="https://gitlab.example.com"
-export EXPORT_DIR=your-local-dir
-```
+   ```bash
+   export PRIVATE_TOKEN=your-access-token
+   export GITLAB_URL="https://gitlab.example.com"
+   export EXPORT_DIR=your-local-dir
+   ```
 
 3. Run the script:
 
-```bash
-bash export.sh
-```
+   ```bash
+   bash export.sh
+   ```
 
 ## GitLab Export/Import Rate Limits
 
 Important: Before running the script, make sure to check the export/import rate limits in GitLab:
 
-* Navigate to: `Admin Area > Settings > Network > Import and export rate limits`
+- Navigate to: `Admin Area > Settings > Network > Import and export rate limits`
 
 By default, GitLab allows no more than 6 export/import requests per minute.
 
@@ -87,14 +86,25 @@ You can monitor the progress in real time via:
 tail -f export.log
 ```
 
+Format:
+
+```java
+[2025-05-30 16:47:34] ────────────────────────────────────────────────────────────
+[2025-05-30 16:47:34] ➡️ Project export <group>/<repo-name> (ID X)
+[2025-05-30 16:47:34] ⏳ Waiting for export: $EXPORT_DIR/<group>/<repo-name> ...
+[2025-05-30 16:47:35] ⌛ <repo-name>: waiting for export 0с...
+[2025-05-30 16:47:40] 📦 Download: $EXPORT_DIR/<group>/<repo-name>.tar.gz
+[2025-05-30 16:47:40] ✅ Export completed: $EXPORT_DIR/<group>/<repo-name>.tar.gz
+```
+
 All actions, including wait states and errors, are logged in export.log.
 
 ## Notes
 
-* Make sure your GitLab token has sufficient permissions (read_api, read_repository).
+- Make sure your GitLab token has sufficient permissions (read_api, read_repository).
 
-* This script performs exports only, not imports.
+- This script performs exports only, not imports.
 
-* Projects with large repositories may take time to export.
+- Projects with large repositories may take time to export.
 
-* The script uses an internal counter and rate control to avoid hitting GitLab API rate limits.
+- The script uses an internal counter and rate control to avoid hitting GitLab API rate limits.
--- export.sh
+++ export.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -e
+set -u
 
 LOG_FILE="export.log"
-MAX_WAIT_SECONDS=180 # 3 minuts
+MAX_WAIT_SECONDS=180 # 3 min
 MAX_EXPORTS_PER_MINUTE=6
 
 # GitLab URL, Token, Export Dir
@@ -15,114 +15,126 @@ export_count=0
 start_time=$(date +%s)
 
 # Checking dependencies
-command -v jq >/dev/null 2>&1 || { echo >&2 "❌ jq is not installed. Install jq."; exit 1; }
-command -v curl >/dev/null 2>&1 || { echo >&2 "❌ curl is not installed. Install curl."; exit 1; }
+command -v jq >/dev/null 2>&1 || {
+	echo >&2 "❌ jq is not installed. Install jq."
+	exit 1
+}
+command -v curl >/dev/null 2>&1 || {
+	echo >&2 "❌ curl is not installed. Install curl."
+	exit 1
+}
 
 log() {
-  local msg="[$(date +'%Y-%m-%d %H:%M:%S')] $1"
-  echo "$msg" | tee -a "$LOG_FILE"
+	local msg
+	msg="[$(date +'%Y-%m-%d %H:%M:%S')] $1"
+	echo "${msg}" | tee -a "${LOG_FILE}"
 }
 
 check_export_rate_limit() {
-  current_time=$(date +%s)
-  elapsed=$((current_time - start_time))
+	current_time=$(date +%s)
+	elapsed=$((current_time - start_time))
 
-  if (( export_count >= MAX_EXPORTS_PER_MINUTE )); then
-    if (( elapsed < 60 )); then
-      log "⏱ Limit reached $MAX_EXPORTS_PER_MINUTE exports/min. Pause $((60 - elapsed + 10)) sec..."
-      sleep $((60 - elapsed + 10))
-    fi
-    export_count=0
-    start_time=$(date +%s)
-  fi
+	if ((export_count >= MAX_EXPORTS_PER_MINUTE)); then
+		if ((elapsed < 60)); then
+			log "⏱ Limit reached ${MAX_EXPORTS_PER_MINUTE} exports/min. Pause $((60 - elapsed + 10)) sec..."
+			sleep $((60 - elapsed + 10))
+		fi
+		export_count=0
+		start_time=$(date +%s)
+	fi
 }
 
 export_project() {
-  local project_id=$1
+	local project_id=$1
 
-  check_export_rate_limit
-  curl --request POST \
-    --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" \
-    "$GITLAB_URL/api/v4/projects/$project_id/export" > /dev/null
-  export_count=$((export_count + 1))
+	check_export_rate_limit
+	curl --request POST \
+		--header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" \
+		"${GITLAB_URL}/api/v4/projects/${project_id}/export" >/dev/null
+	export_count=$((export_count + 1))
 }
 
 download_export() {
-  local project_id=$1
-  local name=$2
-  local path=$3
+	local project_id=$1
+	local name=$2
+	local path=$3
 
-  log "⏳ Waiting for export: $EXPORT_DIR/$path/$name ..."
-  local seconds=0
+	log "⏳ Waiting for export: ${EXPORT_DIR}/${path}/${name} ..."
+	local seconds=0
 
-  while true; do
-    export_status=$(curl --silent --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" \
-      "$GITLAB_URL/api/v4/projects/$project_id/export" | jq -r .export_status)
+	while true; do
+		export_response=$(curl --silent --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" \
+			"${GITLAB_URL}/api/v4/projects/${project_id}/export")
+		export_status=$(echo "${export_response}" | jq -r .export_status)
 
-    if [ "$export_status" == "finished" ]; then
-      mkdir -p "$EXPORT_DIR/$path"
-      log "📦 Download: $EXPORT_DIR/$path/$name.tar.gz"
-      curl --progress-bar --silent --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" \
-        "$GITLAB_URL/api/v4/projects/$project_id/export/download" \
-        --output "$EXPORT_DIR/$path/$name.tar.gz"
-      log "✅ Export completed: $EXPORT_DIR/$path/$name.tar.gz"
-      log ""
-      break
-    elif [ "$export_status" == "failed" ]; then
-      log "❌ Export error: $name"
-      break
-    elif (( seconds >= MAX_WAIT_SECONDS )); then
-      log "⏱ Export timeout ($MAX_WAIT_SECONDS sec): $name. Skipping..."
-      break
-    else
-      if (( seconds % 30 == 0 )); then
-        log "⌛ $name: waiting for export ${seconds}с..."
-      fi
-      sleep 5
-      seconds=$((seconds + 5))
-    fi
-  done
+		if [[ ${export_status} == "finished" ]]; then
+			mkdir -p "${EXPORT_DIR}/${path}"
+			log "📦 Download: ${EXPORT_DIR}/${path}/${name}.tar.gz"
+			curl --progress-bar --silent --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" \
+				"${GITLAB_URL}/api/v4/projects/${project_id}/export/download" \
+				--output "${EXPORT_DIR}/${path}/${name}.tar.gz"
+			log "✅ Export completed: ${EXPORT_DIR}/${path}/${name}.tar.gz"
+			break
+		elif [[ ${export_status} == "failed" ]]; then
+			log "❌ Export error: ${name}"
+			break
+		elif ((seconds >= MAX_WAIT_SECONDS)); then
+			log "⏱ Export timeout (${MAX_WAIT_SECONDS} sec): ${name}. Skipping..."
+			break
+		else
+			if ((seconds % 30 == 0)); then
+				log "⌛ ${name}: waiting for export ${seconds}s..."
+			fi
+			sleep 5
+			seconds=$((seconds + 5))
+		fi
+	done
 }
 
 # Getting all groups taking into account subgroups and pagination
 group_ids=()
 page=1
 while :; do
-  response=$(curl --silent --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" \
-    "$GITLAB_URL/api/v4/groups?all_available=true&per_page=100&page=$page")
-  count=$(echo "$response" | jq 'length')
-  if [ "$count" -eq 0 ]; then break; fi
+	response=$(curl --silent --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" \
+		"${GITLAB_URL}/api/v4/groups?all_available=true&per_page=100&page=${page}")
+	count=$(echo "${response}" | jq 'length')
+	if [[ ${count} -eq 0 ]]; then break; fi
 
-  ids=$(echo "$response" | jq -r '.[].id')
-  group_ids+=($ids)
-  page=$((page + 1))
+	ids=$(echo "${response}" | jq -r '.[].id')
+	group_ids+=("${ids[@]}")
+	page=$((page + 1))
 done
 
 # Processing each group
 for GROUP_ID in "${group_ids[@]}"; do
-  log "📁 Group Processing ID: $GROUP_ID"
+	log "📁 Group Processing ID: ${GROUP_ID}"
 
-  page=1
-  while :; do
-    projects=$(curl --silent --header "PRIVATE-TOKEN: $PRIVATE_TOKEN" \
-      "$GITLAB_URL/api/v4/groups/$GROUP_ID/projects?include_subgroups=true&per_page=100&page=$page")
+	page=1
+	while :; do
+		projects=$(curl --silent --header "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" \
+			"${GITLAB_URL}/api/v4/groups/${GROUP_ID}/projects?include_subgroups=true&per_page=100&page=${page}")
 
-    count=$(echo "$projects" | jq 'length')
-    if [ "$count" -eq 0 ]; then break; fi
+		count=$(echo "${projects}" | jq 'length')
+		if [[ ${count} -eq 0 ]]; then break; fi
+		log "📂 Found ${count} projects on page ${page} for group ID ${GROUP_ID}"
 
-    echo "$projects" | jq -c '.[]' | while read -r project; do
-      id=$(echo "$project" | jq -r .id)
-      name=$(echo "$project" | jq -r .name)
-      path_with_namespace=$(echo "$project" | jq -r .path_with_namespace)
-      namespace_path=$(dirname "$path_with_namespace")
+		if ! project_list=$(echo "${projects}" | jq -c '.[]'); then
+			log "❌ Failed to parse projects JSON."
+			continue
+		fi
 
-      log "────────────────────────────────────────────────────────────"
-      log "➡️ Project export $path_with_namespace (ID $id)"
-      export_project "$id"
-      download_export "$id" "$name" "$namespace_path"
-    done
+		while read -r project; do
+			id=$(echo "${project}" | jq -r .id)
+			name=$(echo "${project}" | jq -r .name)
+			path_with_namespace=$(echo "${project}" | jq -r .path_with_namespace)
+			namespace_path=$(dirname "${path_with_namespace}")
 
-    page=$((page + 1))
-  done
+			log "────────────────────────────────────────────────────────────"
+			log "➡️ Project export ${path_with_namespace} (ID ${id})"
+			export_project "${id}"
+			download_export "${id}" "${name}" "${namespace_path}"
+		done <<<"${project_list}"
+
+		page=$((page + 1))
+	done
 done
-
